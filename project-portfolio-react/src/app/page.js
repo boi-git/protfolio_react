483347db9cd871e053ee3e60995b1cd5a31a5fb1
boi-git <adhwa.aizat23@gmail.com>
2024-11-08 19:29:20 +0800

    Add Project Carousel

--- project-portfolio-react/src/app/page.js
+++ project-portfolio-react/src/app/page.js
@@ -67,7 +67,6 @@ export default function Home() {
           </div>
           <div className="w-full justify-start items-start gap-8 grid grid-cols-12">
             <div className="rounded-[10px] shadow-xl border border-[#c9c9c9] col-span-6 h-fill-available bg-[url('https://via.placeholder.com/485x240')] bg-cover bg-center"></div>
-
             <div className="flex-col justify-center items-start gap-8 inline-flex col-span-6">
               <div className="self-stretch p-8 bg-white rounded-[10px] shadow border border-[#c9c9c9] justify-start items-center inline-flex gap-4">
                 <div className="flex-grow flex-col justify-start items-start inline-flex">
@@ -214,6 +213,53 @@ export default function Home() {
           <div className="flex-col w-full text-center justify-center items-center inline-flex">
             <div className="text-[#3a3a3a] text-[25px] font-normal font-['Poppins'] underline leading-[30px]">
               View More
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full pt-8 justify-start items-center gap-8 inline-flex">
+        <div className="h-[650px] px-[200px] flex-grow py-8 bg-black/50 justify-start items-center gap-8 inline-flex grid grid-cols-12">
+          <div className="h-full col-span-2 flex-col justify-start items-start gap-8 inline-flex overflow-hidden">
+            <div className="w-full flex gap-8 flex-col">
+              <div className="h-[150px] w-full bg-white rounded-[10px]  justify-start items-center inline-flex">
+                <div className="justify-center items-center flex"></div>
+              </div>
+              <div className="h-[150px] w-full bg-white rounded-[10px]  justify-start items-center inline-flex">
+                <div className="justify-center items-center flex"></div>
+              </div>
+              <div className="h-[150px] w-full bg-white rounded-[10px]  justify-start items-center inline-flex">
+                <div className="justify-center items-center flex"></div>
+              </div>
+              <div className="h-[150px] w-full bg-white rounded-[10px]  justify-start items-center inline-flex">
+                <div className="justify-center items-center flex"></div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-10 h-full flex p-8 rounded-[10px] flex-col justify-end items-start inline-flex">
+            <div className="justify-between items-start inline-flex w-full ">
+              <div className="flex-col justify-start items-start gap-2.5 inline-flex ">
+                <div className="flex-col justify-end items-start flex w-full">
+                  <div className="text-white text-[13px] font-normal font-['Poppins'] leading-none">
+                    Project
+                  </div>
+                  <div className="text-white text-[31px] font-normal font-['Poppins'] leading-[37.20px]">
+                    Corsary
+                  </div>
+                </div>
+                <div className=" w-[575px]  text-white text-base font-normal font-['Poppins'] leading-tight">
+                  I'm skilled in translating Figma designs into functional code
+                  with React and Vue, ensuring that my designs are optimized for
+                  performance and usability. This background allows me to build
+                  interfaces that meet user needs and align with business goals.
+                </div>
+              </div>
+              <div className="self-stretch rounded-[10px] justify-center items-center gap-8 flex">
+                <div className="w-12 h-[47.21px] relative" />
+                <div className="w-12 h-12 relative">
+                  <div className="w-12 h-12 left-0 top-0 absolute bg-[#d9d9d9]" />
+                </div>
+                <div className="w-12 h-[30.39px] relative" />
+              </div>
             </div>
           </div>
         </div>
